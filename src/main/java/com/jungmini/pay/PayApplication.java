977b6338e5cfa9dabacf397f833522afa9285bae
김정민 <kjungmin0601@gmail.com>
2023-03-19 14:51:36 +0900

    Initial commit

---
+++ src/main/java/com/jungmini/pay/PayApplication.java
@@ -0,0 +1,13 @@
+package com.jungmini.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
