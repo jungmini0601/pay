610f636046413037610e2ecd6d89ae8635e504bd
김정민 <kjungmin0601@gmail.com>
2023-03-22 19:55:16 +0900

    Refector PasswordEncoder
    encode 메소드 시그니처 예외 제거

--- src/main/java/com/jungmini/pay/service/PasswordEncoder.java
+++ src/main/java/com/jungmini/pay/service/PasswordEncoder.java
@@ -3,7 +3,7 @@ package com.jungmini.pay.service;
 import java.security.NoSuchAlgorithmException;
 
 public interface PasswordEncoder {
-    String encode(String plainText) throws NoSuchAlgorithmException;
+    String encode(String plainText);
 
     boolean verify(String plainText, String encodedText);
 }
