ca7084c14a5eae7fc9d4ea20e7cfcd3259629607
김정민 <kjungmin0601@gmail.com>
2023-03-24 20:41:02 +0900

    style 리뷰 반영

--- src/main/java/com/jungmini/pay/controller/dto/MemberDTO.java
+++ src/main/java/com/jungmini/pay/controller/dto/MemberDTO.java
@@ -3,7 +3,6 @@ package com.jungmini.pay.controller.dto;
 import com.jungmini.pay.domain.Member;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -63,9 +62,13 @@ public class MemberDTO {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SigninMemberRequest {
-
+        @Email(message = "이메일 형식으로 입력 해주세요")
+        @NotBlank(message = "이름은 필수 입력 값입니다.")
+        @Size(min = 3, max = 255, message = "이메일은 3~255자리 문자열 입니다.")
         private String email;
 
+        @NotBlank(message = "이름은 필수 입력 값입니다.")
+        @Size(min = 3, max = 255, message = "비밀번호는 3~255자리 문자열 입니다.")
         private String password;
 
         public Member toEntity() {
