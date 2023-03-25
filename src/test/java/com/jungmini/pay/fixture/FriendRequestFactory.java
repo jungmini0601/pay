7b19fd781a90bfd74494c0ddcbe91801b0f671e9
김정민 <kjungmin0601@gmail.com>
2023-03-25 18:52:28 +0900

    test 친구 신청 Service 테스트

---
+++ src/test/java/com/jungmini/pay/fixture/FriendRequestFactory.java
@@ -0,0 +1,21 @@
+package com.jungmini.pay.fixture;
+
+import com.jungmini.pay.domain.FriendRequest;
+import com.jungmini.pay.domain.Member;
+
+public class FriendRequestFactory {
+
+    public static FriendRequest friendRequest() {
+        Member requester = Member.builder()
+                .email("requester@test.com")
+                .name("requester")
+                .password("tset")
+                .build();
+
+        Member recipient = Member.builder()
+                .email("recipient@test.com")
+                .build();
+
+        return FriendRequest.from(requester, recipient);
+    }
+}
