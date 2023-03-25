3574f9e6be0ad0f3896a0e1a4d3c224dfbd73b1a
김정민 <kjungmin0601@gmail.com>
2023-03-25 19:27:45 +0900

    refector 불필요한 의존성 제거

--- src/test/java/com/jungmini/pay/controller/MemberControllerTest.java
+++ src/test/java/com/jungmini/pay/controller/MemberControllerTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
