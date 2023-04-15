2677c1aad08b4ae613b3e1032f1cc51f525293ce
김정민 <kjungmin0601@gmail.com>
2023-04-15 22:33:55 +0900

    hotfix: redis 설정 변경

--- src/main/java/com/jungmini/pay/common/configuration/LocalRedisConfig.java
+++ src/main/java/com/jungmini/pay/common/configuration/LocalRedisConfig.java
@@ -2,7 +2,11 @@ package com.jungmini.pay.common.configuration;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.embedded.RedisServer;
 
@@ -24,5 +28,12 @@ public class LocalRedisConfig {
     @PreDestroy
     public void stopRedis() {
         this.redisServer.stop();
+    }
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        return Redisson.create(config);
     }
 }
